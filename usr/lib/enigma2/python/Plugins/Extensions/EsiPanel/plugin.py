e2d3af94a2d7e751620ce9fec6adaa71a24c8763
openesi <openesi.eu@gmail.com>
2017-11-18 17:08:00 +0100

    Fix cron

--- usr/lib/enigma2/python/Plugins/Extensions/EsiPanel/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/EsiPanel/plugin.py
@@ -96,7 +96,6 @@ from Plugins.Extensions.EsiPanel.CamCheck import *
 from Plugins.Extensions.EsiPanel.CronManager import *
 #from Plugins.Extensions.EsiPanel.sundtek import *
 from Plugins.Extensions.EsiPanel.SwapManager import Swap, SwapAutostart
-from Plugins.Extensions.Infopanel.SoftwarePanel import SoftwarePanel
 from Plugins.Extensions.EsiPanel.MountManager import *
 
 SystemInfo["SoftCam"] = Check_Softcam()
