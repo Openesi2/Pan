cfbc0319a440b4cae1785a49cc4192419c7c7e44
OpenEsi2 <katrame_@hotmail.com>
2020-02-29 15:36:08 +0100

    start

--- usr/lib/enigma2/python/Plugins/Extensions/EsiPanel/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/EsiPanel/plugin.py
@@ -357,7 +357,7 @@ class EsiPanel(Screen, InfoBarPiP, ProtectedScreen):
                         self.Mlist.append(MenuEntryItem((InfoEntryComponent('SoftcamPanelSetup'), _("Softcam-Panel Setup"), 'Softcam-Panel Setup')))
                 #self.Mlist.append(MenuEntryItem((InfoEntryComponent ("ExtraAddonss" ), _("ExtraAddonss"), ("ExtraAddonss"))))
 
-                #self.Mlist.append(MenuEntryItem((InfoEntryComponent ("SetPiconPath" ), _("SetPiconPath"), ("SetPiconPath"))))
+                self.Mlist.append(MenuEntryItem((InfoEntryComponent ("SetPiconPath" ), _("SetPiconPath"), ("SetPiconPath"))))
                 #self.Mlist.append(MenuEntryItem((InfoEntryComponent ("SoftwareManager" ), _("Software Manager"), ("software-manager"))))
                 self.Mlist.append(MenuEntryItem((InfoEntryComponent('IPKInstaller'), _("IPK Installer"), 'IPKInstaller')))
                 self.Mlist.append(MenuEntryItem((InfoEntryComponent('CronManager'), _("CronManager"), 'CronManager')))  
